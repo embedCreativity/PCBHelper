1cd4690c0c4698d363f30b1ab8cb3d90d1062cbb
Mark Taylor <linux718@gmail.com>
2014-11-26 21:54:34 -0500

    Added get file dialog on Config->Select Database option.  Added sqlite3 support.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,5 +16,31 @@ MainWindow::~MainWindow()
 void MainWindow::on_actionExit_triggered()
 {
     // Clean up before exit
+    sqlite3_close(connDB);
+
     exit(0);
+}
+
+void MainWindow::on_actionSelect_Database_triggered()
+{
+    int sqlReturn;
+
+    do
+    {
+        pathToSqlDb = QFileDialog::getOpenFileName(this, tr("Select Digikey Database"), "/home",
+           tr("Databases (*.sl3)"));
+
+        if ( pathToSqlDb == NULL )
+        {
+            break;
+        }
+
+        // set up handle to the sqlite database
+        sqlite3_close(connDB);
+        sqlReturn = sqlite3_open(pathToSqlDb.toStdString().c_str(), &connDB);
+        if (sqlReturn) {
+            exit(0);
+        }
+    } while ( sqlReturn );
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <sqlite3.h>
 
 namespace Ui {
 class MainWindow;
@@ -18,8 +20,12 @@ public:
 private slots:
     void on_actionExit_triggered();
 
+    void on_actionSelect_Database_triggered();
+
 private:
     Ui::MainWindow *ui;
+    sqlite3 *connDB; // connection object to database
+    QString pathToSqlDb;
 };
 
 #endif // MAINWINDOW_H
