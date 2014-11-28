28b85dcbcd8002dde413dc63493e9701088edf3c
Mark Taylor <linux718@gmail.com>
2014-11-28 16:12:09 -0500

    Display hiding and database querying looking good.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,191 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+DatabaseEntry::DatabaseEntry(void)
+{
+
+}
+
+DatabaseEntry::~DatabaseEntry(void)
+{
+
+}
+
+/*
+ * class DatabaseEntry
+{
+public:
+
+    // class functions
+
+    // data members
+    int id;
+    QString type;
+    QString value;
+    QString package;
+    QString tolerance;
+    QString partnum;
+    QString comments;
+};*/
+// sqlite callback
+int MainWindow::callback(void *db, int argc, char **argv, char **azColName){
+    int i;
+    QString ColName;
+    QString Value;
+    DatabaseEntry dbEntry;
+    QList<DatabaseEntry> *pDbContents = (QList<DatabaseEntry>*)db;
+    // db = QVector dbContents
+    //fprintf(stderr, "%s: ", (const char*)data);
+
+    for(i=0; i<argc; i++){
+        //printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        ColName = azColName[i];
+        Value = argv[i] ? argv[i] : "NULL";
+
+        if ( ColName == "id" )
+        {
+            dbEntry.id = Value.toInt();
+        }
+        else if ( ColName == "type" )
+        {
+            dbEntry.type = Value;
+        }
+        else if ( ColName == "value" )
+        {
+            dbEntry.value = Value;
+        }
+        else if ( ColName == "package" )
+        {
+            dbEntry.package = Value;
+        }
+        else if ( ColName == "tolerance" )
+        {
+            dbEntry.tolerance = Value;
+        }
+        else if ( ColName == "partnum" )
+        {
+            dbEntry.partnum = Value;
+        }
+        else if ( ColName == "comments" )
+        {
+            dbEntry.comments = Value;
+        }
+    } // end for
+
+    pDbContents->push_back(dbEntry);
+    return 0;
+}
+
+void MainWindow::setVisiblePrimaryBrowseTabItems(LabelVisibilty visibility)
+{
+    // primary items
+    switch ( visibility ) {
+        case(HELPER):
+            ui->lbl_helper->setVisible(true);
+            ui->lbl_comments->setVisible(false);
+            ui->lbl_package->setVisible(false);
+            ui->lbl_partnum->setVisible(false);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(false);
+            ui->lbl_type->setVisible(false);
+            ui->lbl_value->setVisible(false);
+            ui->comboBox_package->setVisible(false);
+            ui->comboBox_partnum->setVisible(false);
+            ui->comboBox_tolerance->setVisible(false);
+            ui->comboBox_type->setVisible(false);
+            ui->comboBox_value->setVisible(false);
+            ui->txtComments->setVisible(false);
+            break;
+        case(TYPE):
+            ui->lbl_helper->setVisible(false);
+            ui->lbl_comments->setVisible(false);
+            ui->lbl_package->setVisible(false);
+            ui->lbl_partnum->setVisible(false);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(false);
+            ui->lbl_type->setVisible(true);
+            ui->lbl_value->setVisible(false);
+            ui->comboBox_package->setVisible(false);
+            ui->comboBox_partnum->setVisible(false);
+            ui->comboBox_tolerance->setVisible(false);
+            ui->comboBox_type->setVisible(true);
+            ui->comboBox_value->setVisible(false);
+            ui->txtComments->setVisible(false);
+            break;
+        case(VALUE):
+            ui->lbl_helper->setVisible(false);
+            ui->lbl_comments->setVisible(false);
+            ui->lbl_package->setVisible(false);
+            ui->lbl_partnum->setVisible(false);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(false);
+            ui->lbl_type->setVisible(true);
+            ui->lbl_value->setVisible(true);
+            ui->comboBox_package->setVisible(false);
+            ui->comboBox_partnum->setVisible(false);
+            ui->comboBox_tolerance->setVisible(false);
+            ui->comboBox_type->setVisible(true);
+            ui->comboBox_value->setVisible(true);
+            ui->txtComments->setVisible(false);
+            break;
+        case(PACKAGE):
+            ui->lbl_helper->setVisible(false);
+            ui->lbl_comments->setVisible(false);
+            ui->lbl_package->setVisible(true);
+            ui->lbl_partnum->setVisible(false);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(false);
+            ui->lbl_type->setVisible(true);
+            ui->lbl_value->setVisible(true);
+            ui->comboBox_package->setVisible(true);
+            ui->comboBox_partnum->setVisible(false);
+            ui->comboBox_tolerance->setVisible(false);
+            ui->comboBox_type->setVisible(true);
+            ui->comboBox_value->setVisible(true);
+            ui->txtComments->setVisible(false);
+            break;
+        case(TOLERANCE):
+            ui->lbl_helper->setVisible(false);
+            ui->lbl_comments->setVisible(false);
+            ui->lbl_package->setVisible(true);
+            ui->lbl_partnum->setVisible(false);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(true);
+            ui->lbl_type->setVisible(true);
+            ui->lbl_value->setVisible(true);
+            ui->comboBox_package->setVisible(true);
+            ui->comboBox_partnum->setVisible(false);
+            ui->comboBox_tolerance->setVisible(true);
+            ui->comboBox_type->setVisible(true);
+            ui->comboBox_value->setVisible(true);
+            ui->txtComments->setVisible(false);
+            break;
+        case(PARTNUM):
+            ui->lbl_helper->setVisible(false);
+            ui->lbl_comments->setVisible(true);
+            ui->lbl_package->setVisible(true);
+            ui->lbl_partnum->setVisible(true);
+            ui->lbl_select->setVisible(false);
+            ui->lbl_tolerance->setVisible(true);
+            ui->lbl_type->setVisible(true);
+            ui->lbl_value->setVisible(true);
+            ui->comboBox_package->setVisible(true);
+            ui->comboBox_partnum->setVisible(true);
+            ui->comboBox_tolerance->setVisible(true);
+            ui->comboBox_type->setVisible(true);
+            ui->comboBox_value->setVisible(true);
+            ui->txtComments->setVisible(true);
+            break;
+    } // end switch
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setVisiblePrimaryBrowseTabItems(HELPER);
 }
 
 MainWindow::~MainWindow()
@@ -24,6 +204,8 @@ void MainWindow::on_actionExit_triggered()
 void MainWindow::on_actionSelect_Database_triggered()
 {
     int sqlReturn;
+    int rc;
+    char *zErrMsg = NULL;
 
     do
     {
@@ -38,9 +220,32 @@ void MainWindow::on_actionSelect_Database_triggered()
         // set up handle to the sqlite database
         sqlite3_close(connDB);
         sqlReturn = sqlite3_open(pathToSqlDb.toStdString().c_str(), &connDB);
-        if (sqlReturn) {
-            exit(0);
+        if ( sqlReturn )
+        {
+            continue;
+        }
+
+        /* Execute SQL statement */
+        rc = sqlite3_exec(connDB, "select * from parts", callback, &dbContents, &zErrMsg);
+        if( rc != SQLITE_OK ){
+            printf("SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+            sqlite3_close(connDB);
+            sqlReturn = 1; // force loop repeat
+        }else{
+            printf("Operation done successfully\n");
+            break;
         }
     } while ( sqlReturn );
 
+    QList<DatabaseEntry>::iterator i;
+    for (i = dbContents.begin(); i != dbContents.end(); ++i) {
+        ui->comboBox_type->addItem((*i).type);
+    }
+    // Enable all items in form window
+    setVisiblePrimaryBrowseTabItems(TYPE);
 }
+
+
+/******************** EOF **************************/
+
--- mainwindow.h
+++ mainwindow.h
@@ -4,16 +4,46 @@
 #include <QMainWindow>
 #include <QFileDialog>
 #include <sqlite3.h>
+#include <QList>
 
 namespace Ui {
+class DatabaseEntry;
 class MainWindow;
 }
+// CREATE TABLE parts ( id integer primary key,
+//  type text, value text, package text, tolerance text, partnum text, comments text);
+class DatabaseEntry
+{
+public:
+
+    // class functions
+    DatabaseEntry();
+    ~DatabaseEntry();
+
+    // data members
+    int id;
+    QString type;
+    QString value;
+    QString package;
+    QString tolerance;
+    QString partnum;
+    QString comments;
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    typedef enum {
+        HELPER = 0,
+        TYPE,
+        VALUE,
+        PACKAGE,
+        TOLERANCE,
+        PARTNUM
+    } LabelVisibilty;
+
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
@@ -23,9 +53,14 @@ private slots:
     void on_actionSelect_Database_triggered();
 
 private:
+    void setVisiblePrimaryBrowseTabItems(LabelVisibilty);
     Ui::MainWindow *ui;
     sqlite3 *connDB; // connection object to database
     QString pathToSqlDb;
+    QList<DatabaseEntry> dbContents;
+
+    // sqlite callback
+    static int callback(void *data, int argc, char **argv, char **azColName);
 };
 
 #endif // MAINWINDOW_H
